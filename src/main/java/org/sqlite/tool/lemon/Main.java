44d2cd3e70c733cc07d0be523b541c2092c7da62
little-pan <pzp@maihesoft.com>
2019-08-04 18:58:47 +0800

    Init the mvn project

---
+++ src/main/java/org/sqlite/tool/lemon/Main.java
@@ -0,0 +1,16 @@
+package org.sqlite.tool.lemon;
+
+/**
+ * The main program.  Parse the command line and do it。
+ * 
+ * @author little-pan
+ * @since 2019-08-04
+ *
+ */
+public class Main {
+    
+    public static void main(String[] args) {
+        
+    }
+
+}
